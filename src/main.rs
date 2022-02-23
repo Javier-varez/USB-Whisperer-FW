38efb7ed706e5e400f47a075b75399f810f28f5c
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2022-02-23 19:59:49 +0100

    Configure ISR for FUSB302 IRQ
    Instead of processing the IRQs at the regular fsm rate, let's try to
    respond to them as soon as possible and schedule the fsm task as soon as
    we receive the irq.

--- src/main.rs
+++ src/main.rs
@@ -25,26 +25,31 @@ pub fn exit() -> ! {
 #[rtic::app(device = nrf52840_hal::pac, dispatchers = [NFCT])]
 mod app {
     use super::{fusb302, usb_fsm};
-    use nrf52840_hal as hal;
+    use nrf52840_hal::{self as hal, gpiote::Gpiote};
     use systick_monotonic::*;
-    
-    use hal::{Twim, pac::{TIMER0, TWIM0}, gpio::{Pin, Input, Output, PullUp, PushPull}, timer::Timer};
-    
-    defmt::timestamp!("{=u64:us}", {
-        monotonics::now().ticks() * 1000
-    });
 
-    
+    use hal::{
+        gpio::{Input, Output, Pin, PullUp, PushPull},
+        pac::{TIMER0, TWIM0},
+        timer::Timer,
+        Twim,
+    };
+
+    defmt::timestamp!("{=u64:us}", { monotonics::now().ticks() * 1000 });
+
     // Local resources to specific tasks (cannot be shared)
     #[local]
     struct Local {
         state_machine: usb_fsm::UsbFsm<Twim<TWIM0>, Pin<Input<PullUp>>, Pin<Output<PushPull>>>,
-        timer: Timer<TIMER0>
+        timer: Timer<TIMER0>,
+        gpiote: Gpiote,
     }
 
     // Resources shared between tasks
     #[shared]
-    struct Shared {}
+    struct Shared {
+        fsm_spawn_handle: Option<run_state_machine::SpawnHandle>,
+    }
 
     #[monotonic(binds = SysTick, default = true, priority = 7)]
     type SystickMonotonic = Systick<1000>; // 1000 Hz / 1 ms granularity
@@ -76,12 +81,29 @@ mod app {
 
         let timer = hal::timer::Timer::new(peripherals.TIMER0);
 
+        let gpiote = hal::gpiote::Gpiote::new(peripherals.GPIOTE);
+        gpiote
+            .channel0()
+            .input_pin(&irq_pin)
+            .hi_to_lo()
+            .enable_interrupt();
+
         let fusb302 = fusb302::Fusb302::new(twim).unwrap();
         let state_machine = usb_fsm::UsbFsm::new(fusb302, irq_pin, vbus_pin).unwrap();
-        
+
         run_state_machine::spawn().unwrap();
 
-        (Shared {}, Local {state_machine, timer}, init::Monotonics(mono))
+        (
+            Shared {
+                fsm_spawn_handle: None,
+            },
+            Local {
+                state_machine,
+                timer,
+                gpiote,
+            },
+            init::Monotonics(mono),
+        )
     }
 
     #[idle]
@@ -90,15 +112,39 @@ mod app {
             cortex_m::asm::wfi();
         }
     }
-    
-    #[task(local = [state_machine, timer], shared = [])]
+
+    #[task(local = [gpiote], shared = [fsm_spawn_handle], binds = GPIOTE, priority = 2)]
+    fn gpiote_irq(cx: gpiote_irq::Context) {
+        // We need to handle the IRQ here and let the state machine now it needs to run
+        let gpiote = cx.local.gpiote;
+        let mut spawn_handle = cx.shared.fsm_spawn_handle;
+
+        if gpiote.channel0().is_event_triggered() {
+            defmt::info!("IRQ: FUSB302");
+            // Cancel pending state machine run if any
+            spawn_handle.lock(|handle| {
+                if let Some(handle) = handle.take() {
+                    handle.cancel().ok();
+                }
+            });
+
+            run_state_machine::spawn().unwrap();
+            gpiote.channel0().reset_events();
+        }
+    }
+
+    #[task(local = [state_machine, timer], shared = [fsm_spawn_handle], capacity = 1, priority = 1)]
     fn run_state_machine(cx: run_state_machine::Context) {
-        let timer = cx.local.timer;
-        let state_machine = cx.local.state_machine;
-        
+        let run_state_machine::LocalResources {
+            timer,
+            state_machine,
+        } = cx.local;
+        let mut spawn_handle = cx.shared.fsm_spawn_handle;
+
         state_machine.run(timer).unwrap();
-        
+
         // Spawn again after 4 ms
-        run_state_machine::spawn_after(4.millis()).unwrap();
-    }    
+        let handle = run_state_machine::spawn_after(4.millis()).unwrap();
+        spawn_handle.lock(|hdl| hdl.replace(handle));
+    }
 }
